254e53d025655f0159842f12e45d8a24fc4d19c4
Swapnanil Dutta <47251193+swapnanildutta@users.noreply.github.com>
2020-04-07 13:18:11 +0530

    Add files via upload

---
+++ ig_data/InstaSearch.py
@@ -0,0 +1,38 @@
+# imports
+import requests, json
+# beautifulsoup4
+from bs4 import BeautifulSoup
+
+def searchDisplay(username):
+    # base url for the data
+    url = 'https://www.instagram.com/{}/'.format(username)
+    try:
+        req = requests.get(url).content
+        soup=BeautifulSoup(req,"html.parser")
+        row=soup.find_all('script')
+        details=row[3].text
+        account=json.loads(details)
+        try:
+            if len(account['description'])<1:
+                account['description']=""
+        except:
+            account['description']=""
+        print("Name : ",account['name'],'\t',"Username : ",account['alternateName'],
+              '\t',"Followers : ",account['mainEntityofPage']['interactionStatistic']['userInteractionCount'],'\n',
+              "Bio : ",account['description'])
+    except:
+        print('Not found or no internet connection')
+
+def getDetails(username):
+    url = 'https://www.instagram.com/{}/'.format(username)
+    try:
+        req = requests.get(url).content
+        soup=BeautifulSoup(req,"html.parser")
+        row=soup.find_all('script')
+        details=row[3].text
+        account=json.loads(details)
+        return account
+    except:
+        print('Not found or no internet connection')
+        return {}
+
