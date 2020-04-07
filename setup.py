eea6a7434f7b22100b5e0187f41bd05e71821cd9
Swapnanil Dutta <47251193+swapnanildutta@users.noreply.github.com>
2020-04-07 14:31:09 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ig_data-swapnanildutta",
+    version="0.0.1",
+    author="Swapnanil Dutta",
+    author_email="swapnanildutta2000@gmail.com",
+    description="A small package to extract instagram data for the given username",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/swapnanildutta/instagram-search",
+    download_url="ig_data-swapnanildutta-0.0.1.tar.gz",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
